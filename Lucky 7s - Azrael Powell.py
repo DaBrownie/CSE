5641324c019aa582091a1184f891d57d87e4558e
DaBrownie <31044530+DaBrownie@users.noreply.github.com>
2018-01-11 10:47:37 -0800

    1.11.18

--- Lucky 7s - Azrael Powell.py
+++ Lucky 7s - Azrael Powell.py
@@ -2,37 +2,38 @@ import random
 round = 0
 money = 15
 broke = False
+tm = 0  # tm stands for "Top Money"
+tr = 0  # tr stands for "Top Round"
 
 while money > 0 and broke is False:
-    # question = input("Do you want to roll the dice?")
-    # if question == "yes":
-    die_1 = random.randint(1, 6)
-    die_2 = random.randint(1, 6)
-    if die_1 + die_2 == 7:
-        print("Congratz you have gained $4 and have %s" % money)
-        print("You are on round %s" % round)
-        print("You rolled %d" % (die_2 + die_1))
-        round += 1
+    D1 = random.randint(1, 6)
+    D2 = random.randint(1, 6)
+    if D1 + D2 == 7:
+        print("You rolled %d" % (D2 + D1))
         money += 4
-    elif die_1 + die_2 < 7:
-        print("Try again you have lost $1 and have %s" % money)
-        print("You are on round %s" % round)
-        print("You rolled %d" % (die_2 + die_1))
-        print(money - 1)
-        print(round + 1)
-        money -= 1
+        print("Congratz you have gained $4 and have $%s" % money)
         round += 1
+        print("You are on round %s" % round)
+        print()
+        # if money > 15:
+        #     question = input("Do you want to stop?")
+        #     if question == "yes":
+        #         broke = True
+        #         print("You got to round %s" % round)
+        #     if question == "no":
+        #         broke = False
+    elif D1 + D2 != 7:
+        print("You rolled %d" % (D2 + D1))
+        money -= 1
+        print("You have lost $1 and have $%s" % money)
+        round += 1
+        print("You are on round %s" % round)
+        print()
         if money == 0:
             print("You are out of money")
+            print("You got to %s" % round)
             broke = True
-    elif die_1 + die_2 > 7:
-        print("Try again you have lost $1 and have %s" % money)
-        print("You are on round %s" % round)
-        print("You rolled %d" % (die_2 + die_1))
-        print(money - 1)
-        print(round + 1)
-        money -= 1
-        round += 1
-        if money == 0:
-            print("You are out of money")
-            broke = True
+            print("You should have stopped at round %s when you had $%s" % (tr,tm))
+    if money > tm:
+        tm = money
+        tr = round
